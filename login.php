4763ef65a80792566770f529d70a5e721e3359c2
ApoorvaAntappa <apoorvaantappa123@gmail.com>
2025-04-04 23:29:21 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -1,6 +1,12 @@
 <?php
 
-include '../components/connect.php';
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
 
 if(isset($_POST['submit'])){
 
@@ -9,20 +15,28 @@ if(isset($_POST['submit'])){
    $pass = sha1($_POST['pass']);
    $pass = filter_var($pass, FILTER_SANITIZE_STRING);
 
-   $select_tutor = $conn->prepare("SELECT * FROM `tutors` WHERE email = ? AND password = ? LIMIT 1");
-   $select_tutor->execute([$email, $pass]);
-   $row = $select_tutor->fetch(PDO::FETCH_ASSOC);
+   $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ? AND password = ? LIMIT 1");
+   $select_user->execute([$email, $pass]);
+   $row = $select_user->fetch(PDO::FETCH_ASSOC);
    
-   if($select_tutor->rowCount() > 0){
-     setcookie('tutor_id', $row['id'], time() + 60*60*24*30, '/');
-     header('location:dashboard.php');
+   if($select_user->rowCount() > 0){
+     setcookie('user_id', $row['id'], time() + 60*60*24*30, '/');
+     header('location:home.php');
    }else{
       $message[] = 'incorrect email or password!';
    }
 
 }
 
+
 ?>
+
+
+
+
+
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -30,22 +44,25 @@ if(isset($_POST['submit'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Login</title>
+   <title>TechTube - All in one educational platform</title>
 
    <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+   <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="../css/admin_style.css">
-   <link rel="icon" href="../images/favicon.png" type="image/x-icon">
-</head>
-<body style="padding-left: 0;">
+   <link rel="stylesheet" href="css/style.css">
+   <!-- favicon link  -->
+   <link rel="icon" href="images/favicon.png" type="image/x-icon">
+  
+   
 
+</head>
+<body>
 <?php
 if(isset($message)){
    foreach($message as $message){
       echo '
-      <div class="message form">
+      <div class="message">
          <span>'.$message.'</span>
          <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
       </div>
@@ -53,60 +70,87 @@ if(isset($message)){
    }
 }
 ?>
+<nav class="nav">
 
-<!-- register section starts  -->
+   <section class="flex">
 
+   <div class="main_logo">
+         <i class="uil uil-airplay"></i>
+         <a href="home.php" class="logo">TechTube</a>
+      </div>
+
+      <form action="search_course.php" method="post" class="search-form">
+         <input type="text" name="search_course" placeholder="search courses..." required maxlength="100">
+         <button type="submit" class="fas fa-search" name="search_course_btn"></button>
+      </form>
+      <div class="navmenu">
+         <a class="nav__link active-link" href="home.php"></i><span>Home</span></a>
+      <a class="nav__link" href="about.php"></i><span>About</span></a>
+      <a class="nav__link" href="courses.php"></i><span>Courses</span></a>
+      <a class="nav__link" href="teachers.php"></i><span>Teachers</span></a>
+      <a class="nav__link" href="contact.php"></i><span>Contact us</span></a>
+      </div>
+
+      <div class="icons">
+         
+         <div style="display: none;" id="search-btn" class="fas fa-search"></div>
+         <div style="display: none;" id="user-btn" class="fas fa-user"></div>
+
+  
+         <div id="menu-btn" class="fas fa-bars"></div>
+         <div id="close-btn" class="fas fa-times"></div>
+         <div id="toggle-btn"class="fas fa-sun"></div>
+      </div>
+
+      <div class="profile">
+         <?php
+            $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+            $select_profile->execute([$user_id]);
+            if($select_profile->rowCount() > 0){
+            $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+         ?>
+         <img class="image" src="uploaded_files/<?= $fetch_profile['image']; ?>" alt="">
+         <h3 class="prof"><?= $fetch_profile['name']; ?></h3>
+         <span class="student">Student</span>
+         <a href="profile.php" class="btn" style =" background-color: #16a085">View profile</a>
+         <a href="components/user_logout.php" onclick="return confirm('logout from this website?');" class="delete-btn">Logout</a>
+         <?php
+            }else{
+         ?>
+         <h3>Please login or register</h3>
+          <div class="flex-btn">
+            <a href="login.php" class="option-btn">login</a>
+            <a href="register.php" class="option-btn">register</a>
+         </div>
+         <?php
+            }
+         ?>
+      </div>
+
+   </section>
+</nav>
 <section class="form-container">
-
-   <form action="" method="post" enctype="multipart/form-data" class="login">
-      <h3>welcome back!</h3>
-      <p>your email <span>*</span></p>
-      <input type="email" name="email" placeholder="enter your email" maxlength="20" required class="box">
-      <p>your password <span>*</span></p>
-      <input type="password" name="pass" placeholder="enter your password" maxlength="20" required class="box">
-      <p class="link">Don't have an account? <a href="register.php">Register Now</a></p>
-      <input type="submit" name="submit" value="login now" class="btn">
+    <div class="welcome">
+      <h1 >Hello, Again!</h1>
+      <p>We're delighted to see you return. Reconnect with your personalized dashboard, track your progress, and resume your learning journey seamlessly. Login now and let's continue this inspiring journey together.
+      </p>
+      <img src="images/books.png" alt="">
+    </div>
+    <form action="" method="post" enctype="multipart/form-data" class="login">
+      <h3>User Login</h3>
+      <div class="form-box">
+         <input type="email" name="email" placeholder="Enter your email" maxlength="50" required class="box">
+         <i class="uil uil-envelope"></i>
+      </div>
+      <div class="form-box">
+         <input type="password" name="pass" placeholder=" Enter your password" maxlength="20" required class="box">
+         <i class="uil uil-padlock"></i>
+      </div>
+      <label for=""><input type="checkbox">Remember password</label>
+      <input type="submit" name="submit" value="Login now" class="btnn btn">
+      <p class="link">don't have an account? <a href="register.php">Register now</a></p>
    </form>
-
-</section>
-
-<!-- registe section ends -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<script>
-
-let darkMode = localStorage.getItem('dark-mode');
-let body = document.body;
-
-const enabelDarkMode = () =>{
-   body.classList.add('dark');
-   localStorage.setItem('dark-mode', 'enabled');
-}
-
-const disableDarkMode = () =>{
-   body.classList.remove('dark');
-   localStorage.setItem('dark-mode', 'disabled');
-}
-
-if(darkMode === 'enabled'){
-   enabelDarkMode();
-}else{
-   disableDarkMode();
-}
-
-</script>
-   
-</body>
+   </section>
+    <script src="js/app.js"></script>
+  </body>
 </html>
--- register.php
+++ register.php
@@ -1,14 +1,18 @@
 <?php
 
-include '../components/connect.php';
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
 
 if(isset($_POST['submit'])){
 
    $id = unique_id();
    $name = $_POST['name'];
    $name = filter_var($name, FILTER_SANITIZE_STRING);
-   $profession = $_POST['profession'];
-   $profession = filter_var($profession, FILTER_SANITIZE_STRING);
    $email = $_POST['email'];
    $email = filter_var($email, FILTER_SANITIZE_STRING);
    $pass = sha1($_POST['pass']);
@@ -22,21 +26,29 @@ if(isset($_POST['submit'])){
    $rename = unique_id().'.'.$ext;
    $image_size = $_FILES['image']['size'];
    $image_tmp_name = $_FILES['image']['tmp_name'];
-   $image_folder = '../uploaded_files/'.$rename;
+   $image_folder = 'uploaded_files/'.$rename;
 
-   $select_tutor = $conn->prepare("SELECT * FROM `tutors` WHERE email = ?");
-   $select_tutor->execute([$email]);
+   $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ?");
+   $select_user->execute([$email]);
    
-   if($select_tutor->rowCount() > 0){
+   if($select_user->rowCount() > 0){
       $message[] = 'email already taken!';
    }else{
       if($pass != $cpass){
          $message[] = 'confirm passowrd not matched!';
       }else{
-         $insert_tutor = $conn->prepare("INSERT INTO `tutors`(id, name, profession, email, password, image) VALUES(?,?,?,?,?,?)");
-         $insert_tutor->execute([$id, $name, $profession, $email, $cpass, $rename]);
+         $insert_user = $conn->prepare("INSERT INTO `users`(id, name, email, password, image) VALUES(?,?,?,?,?)");
+         $insert_user->execute([$id, $name, $email, $cpass, $rename]);
          move_uploaded_file($image_tmp_name, $image_folder);
-         $message[] = 'new tutor registered! please login now';
+         
+         $verify_user = $conn->prepare("SELECT * FROM `users` WHERE email = ? AND password = ? LIMIT 1");
+         $verify_user->execute([$email, $pass]);
+         $row = $verify_user->fetch(PDO::FETCH_ASSOC);
+         
+         if($verify_user->rowCount() > 0){
+            setcookie('user_id', $row['id'], time() + 60*60*24*30, '/');
+            header('location:home.php');
+         }
       }
    }
 
@@ -50,22 +62,23 @@ if(isset($_POST['submit'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>register</title>
+   <title>Register Now - Welcome to TechTube</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+   <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="../css/admin_style.css">
-   <link rel="icon" href="../images/favicon.png" type="image/x-icon">
-</head>
-<body style="padding-left: 0;">
+   <link rel="stylesheet" href="css/style.css">
+   <link rel="icon" href="images/favicon.png" type="image/x-icon">
 
+</head>
+<body>
 <?php
 if(isset($message)){
    foreach($message as $message){
       echo '
-      <div class="message form">
+      <div class="message">
          <span>'.$message.'</span>
          <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
       </div>
@@ -73,51 +86,110 @@ if(isset($message)){
    }
 }
 ?>
+<nav class="nav">
 
-<!-- register section starts  -->
+   <section class="flex">
 
-<section class="form-container">
-
-   <form class="register" action="" enctype="multipart/form-data">
-      <h3>Register</h3>
-      <div class="flex">
-         <div class="col">
-            <p>your name <span>*</span></p>
-            <input type="text" name="name" placeholder="Enter Your Name" maxlength="50" required class="box">
-            <p>your profession <span>*</span></p>
-            <select name="profession" class="box" required>
-               <option value="" disabled selected>-- Select Your Profession</option>
-               <option value="Assistant Professor">Assistant Professor</option>
-               <option value="Data Engineer">Data Engineer</option>
-               <option value="Backend Developer">Backend Developer</option>
-               <option value="UI/UX Designer">UI/UX Designer</option>
-               <option value="Head of Department">Head of Department</option>
-               <option value="Frontend Developer">Frontend Developer</option>
-               <option value="Android Developer">Android Developer</option>
-               <option value="Techtuber">Techtuber</option>
-               <option value="Lecturer">Lecturer</option>
-               <option value="Full Stack Developer">Full Stack Developer</option>
-               <option value="Entrepreneur">Entrepreneur</option>
-            </select>
-            <p>your email <span>*</span></p>
-            <input type="email" name="email" placeholder="Enter Your Email" maxlength="20" required class="box">
-         </div>
-         <div class="col">
-            <p>your password <span>*</span></p>
-            <input type="password" name="pass" placeholder="Enter Your Password" maxlength="20" required class="box">
-            <p>confirm password <span>*</span></p>
-            <input type="password" name="cpass" placeholder="Confirm Your Password" maxlength="20" required class="box">
-            <p>select pic <span>*</span></p>
-            <input type="file" name="image" accept="image/*" required class="box">
-         </div>
+   <div class="main_logo">
+         <i class="uil uil-airplay"></i>
+         <a href="home.php" class="logo">TechTube</a>
       </div>
-      <p class="link">Already have an account? <a href="login.php">Login Now</a></p>
-      <input type="submit" name="submit" value="register now" class="btn">
+
+      <form action="search_course.php" method="post" class="search-form">
+         <input type="text" name="search_course" placeholder="search courses..." required maxlength="100">
+         <button type="submit" class="fas fa-search" name="search_course_btn"></button>
+      </form>
+      <div class="navmenu">
+         <a class="nav__link active-link" href="home.php"></i><span>Home</span></a>
+      <a class="nav__link" href="about.php"></i><span>About</span></a>
+      <a class="nav__link" href="courses.php"></i><span>Courses</span></a>
+      <a class="nav__link" href="teachers.php"></i><span>Teachers</span></a>
+      <a class="nav__link" href="contact.php"></i><span>Contact us</span></a>
+      </div>
+
+      <div class="icons">
+         
+         <div style="display: none;" id="search-btn" class="fas fa-search"></div>
+         <div style="display: none;" id="user-btn" class="fas fa-user"></div>
+
+  
+         <div id="menu-btn" class="fas fa-bars"></div>
+         <div id="close-btn" class="fas fa-times"></div>
+         <div id="toggle-btn"class="fas fa-sun"></div>
+      </div>
+
+      <div class="profile">
+         <?php
+            $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+            $select_profile->execute([$user_id]);
+            if($select_profile->rowCount() > 0){
+            $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+         ?>
+         <img class="image" src="uploaded_files/<?= $fetch_profile['image']; ?>" alt="">
+         <h3 class="prof"><?= $fetch_profile['name']; ?></h3>
+         <span class="student">Student</span>
+         <a href="profile.php" class="btn" style =" background-color: #16a085">View profile</a>
+         <a href="components/user_logout.php" onclick="return confirm('logout from this website?');" class="delete-btn">Logout</a>
+         <?php
+            }else{
+         ?>
+         <h3>Please login or register</h3>
+          <div class="flex-btn">
+            <a href="login.php" class="option-btn">login</a>
+            <a href="register.php" class="option-btn">register</a>
+         </div>
+         <?php
+            }
+         ?>
+      </div>
+
+   </section>
+</nav>
+
+<section  class="form-container">
+   <div class="welcome">
+      <h1 >Welcome to TechTube!</h1>
+      <p>Join our community by creating your account today. Gain access to a world of educational resources, interactive courses, and expert guidance. Let us be your partner on your journey towards academic excellence. Sign up now and embark on a path to limitless learning and growth.
+      </p>
+      <img src="images/books.png" alt="">
+    </div>
+   <form class="register" action="" method="post" enctype="multipart/form-data">
+      <h3>create account</h3>
+      <div class="flex">
+         
+            <div class="form-box">
+               <input type="text" name="name" placeholder="Enter your name" maxlength="50" required class="box">
+               <i class="uil uil-user"></i>
+            </div>
+
+            <div class="form-box">   
+               <input type="email" name="email" placeholder="Enter your email" maxlength="50" required class="box">
+               <i class="uil uil-envelope"></i>
+            </div>
+
+            <div class="form-box"> 
+               <input type="password" name="pass" placeholder="Enter your password" maxlength="20" required class="box">
+               <i class="uil uil-padlock"></i>
+            </div>
+
+            <div class="form-box"> 
+               <input type="password" name="cpass" placeholder="Confirm your password" maxlength="20" required class="box">
+               <i class="uil uil-lock-open-alt"></i>
+            </div>
+            
+            <div class="form-box"> 
+               <input type="file" id="img" name="image" accept="image/*" required class="box" >
+               <i class="uil uil-images"></i>
+            </div>
+      </div>
+     
+      
+      <p class="link">already have an account? <a href="login.php">Login now</a></p>
+      <input type="submit" name="submit" value="Register now" class=" btnn btn">
    </form>
 
 </section>
 
-<!-- registe section ends -->
 
 
 
@@ -130,28 +202,9 @@ if(isset($message)){
 
 
 
-<script>
 
-let darkMode = localStorage.getItem('dark-mode');
-let body = document.body;
-
-const enabelDarkMode = () =>{
-   body.classList.add('dark');
-   localStorage.setItem('dark-mode', 'enabled');
-}
-
-const disableDarkMode = () =>{
-   body.classList.remove('dark');
-   localStorage.setItem('dark-mode', 'disabled');
-}
-
-if(darkMode === 'enabled'){
-   enabelDarkMode();
-}else{
-   disableDarkMode();
-}
-
-</script>
+<!-- custom js file link  -->
+<script src="js/app.js"></script>
    
 </body>
 </html>
